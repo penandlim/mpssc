f9777def31ea6adac472bff0be57d54acdf85720
John Lim <penandlim@gmail.com>
2016-03-09 20:39:26 -0800

    Play MPV on a separate thread

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
+import json
 import os
+import re
+import socket
 import subprocess
+import threading
 import soundcloud
+import time
+
+
+class PlayWithMpv(threading.Thread):
+    def __init__(self, cmd):
+        self.stdout = None
+        self.stderr = None
+        self.cmd = cmd
+        self.p = None
+        threading.Thread.__init__(self)
+
+    def run(self):
+        self.p = subprocess.Popen(cmd,
+                             shell=True,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.STDOUT)
+
+        self.stdout, self.stderr = self.p.communicate()
+
+    def stop(self):
+        self.p.kill()
 
 
 class TerminalColors:
@@ -15,28 +40,6 @@ class TerminalColors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
-
-
-isPlaying = False
-
-__location__ = os.path.realpath(
-    os.path.join(os.getcwd(), os.path.dirname(__file__)))
-
-with open(os.path.join(__location__, '.secret')) as f:
-    cid = f.readline().strip()
-    cs = f.readline().strip()
-    f.close()
-
-un = "username"
-pw = "password"
-
-client = soundcloud.Client(
-    client_id=cid,
-    client_secret=cs
-)
-
-page_size = 20
-tracks = None
 
 
 def which(program):
@@ -66,34 +69,60 @@ def represents_int(s):
         return False
 
 
-while not isPlaying:
-    command = raw_input(TerminalColors.BOLD + "\n> " + TerminalColors.ENDC)
-    if command:
-        if tracks and represents_int(command):
-            try:
-                track = tracks[int(command) - 1]
-                track_url = track.permalink_url
-                track_pic = track.artwork_url
-                track_title = track.title
-                cmd = which('mpv')
-                cmd += ' ' + track_url
-                p = subprocess.Popen([cmd, track_url], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-                print TerminalColors.BOLD + '"' + track_title + '"' + TerminalColors.ENDC + ' playing...'
-                stdout, stderr = p.communicate()
 
-            except IndexError:
-                print 'Please enter a valid number'
+isPlaying = False
 
-        elif command[0] == '/' and len(command) > 1:
-            tracks = client.get('/tracks', q=command[1:], limit=page_size)
-            for i in range(0, len(tracks) - 1):
-                formattedOrderNum = str(i + 1)
-                if len(formattedOrderNum) == 1:
-                    formattedOrderNum = '0' + formattedOrderNum
-                print TerminalColors.OKBLUE + formattedOrderNum + TerminalColors.ENDC + TerminalColors.OKGREEN + ' ' + tracks[
-                    i].title + TerminalColors.ENDC
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-        elif command == 'quit' or command == 'exit':
-            break
-        else:
-            print 'Command not accepted'
+with open(os.path.join(__location__, '.secret')) as f:
+    cid = f.readline().strip()
+    cs = f.readline().strip()
+    f.close()
+
+un = "username"
+pw = "password"
+
+client = soundcloud.Client(
+    client_id=cid,
+    client_secret=cs
+)
+
+page_size = 20
+tracks = None
+
+playWithMpv = None
+
+while True:
+    if not isPlaying:
+        command = raw_input(TerminalColors.BOLD + "\n> " + TerminalColors.ENDC)
+        if command:
+            if tracks and represents_int(command):
+                try:
+                    track = tracks[int(command) - 1]
+                    track_url = track.permalink_url
+                    track_pic = track.artwork_url
+                    track_title = track.title
+                    cmd = which('mpv')
+                    cmd += ' ' + track_url
+                    playWithMpv = PlayWithMpv(cmd)
+                    playWithMpv.start()
+                    print TerminalColors.BOLD + '"' + track_title + '"' + TerminalColors.ENDC + ' playing...'
+
+                except IndexError:
+                    print 'Please enter a valid number'
+
+            elif command[0] == '/' and len(command) > 1:
+                tracks = client.get('/tracks', q=command[1:], limit=page_size)
+                for i in range(0, len(tracks) - 1):
+                    formattedOrderNum = str(i + 1)
+                    if len(formattedOrderNum) == 1:
+                        formattedOrderNum = '0' + formattedOrderNum
+                    print TerminalColors.OKBLUE + formattedOrderNum + TerminalColors.ENDC + TerminalColors.OKGREEN + ' ' + \
+                          tracks[i].title + TerminalColors.ENDC
+
+            elif command == 'quit' or command == 'exit':
+                break
+            else:
+                print 'Command not accepted'
+
