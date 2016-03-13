c0cee3dce814999dcb521bb4dd58a6db7ae49858
John Lim <penandlim@gmail.com>
2016-03-12 17:08:08 -0800

    Display approximate time when playing songs. g.py holds global values.

---
+++ g.py
@@ -0,0 +1,12 @@
+import os
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+un = "username"
+pw = "password"
+p = None
+isPlaying = False
+page_size = 20
+tracks = None
+elapsed_time = None
+playWithMpv = None
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import json
 import os
-import re
-import socket
 import subprocess
 import threading
 import soundcloud
 import time
+
+import sys
+
+import g
 
 
 class PlayWithMpv(threading.Thread):
@@ -13,16 +14,15 @@ class PlayWithMpv(threading.Thread):
         self.stdout = None
         self.stderr = None
         self.cmd = cmd
-        self.p = None
         threading.Thread.__init__(self)
 
     def run(self):
-        self.p = subprocess.Popen(cmd,
-                             shell=True,
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.STDOUT)
+        g.p = subprocess.Popen(cmd,
+                                  shell=True,
+                                  stdout=subprocess.PIPE,
+                                  stderr=subprocess.STDOUT)
 
-        self.stdout, self.stderr = self.p.communicate()
+        self.stdout, self.stderr = g.p.communicate()
 
     def stop(self):
         self.p.kill()
@@ -69,60 +69,78 @@ def represents_int(s):
         return False
 
 
+def fmt_time(seconds):
+    """ Format number of seconds to %H:%M:%S. """
+    hms = time.strftime('%H:%M:%S', time.gmtime(int(seconds)))
+    H, M, S = hms.split(":")
 
-isPlaying = False
+    if H == "00":
+        hms = M + ":" + S
 
-__location__ = os.path.realpath(
-    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    elif H == "01" and int(M) < 40:
+        hms = str(int(M) + 60) + ":" + S
 
-with open(os.path.join(__location__, '.secret')) as f:
+    elif H.startswith("0"):
+        hms = ":".join([H[1], M, S])
+
+    return hms
+
+
+with open(os.path.join(g.__location__, '.secret')) as f:
     cid = f.readline().strip()
     cs = f.readline().strip()
     f.close()
-
-un = "username"
-pw = "password"
 
 client = soundcloud.Client(
     client_id=cid,
     client_secret=cs
 )
 
-page_size = 20
-tracks = None
-
-playWithMpv = None
-
 while True:
-    if not isPlaying:
-        command = raw_input(TerminalColors.BOLD + "\n> " + TerminalColors.ENDC)
+    if not g.isPlaying:
+        command = input(TerminalColors.BOLD + "\n> " + TerminalColors.ENDC)
         if command:
-            if tracks and represents_int(command):
+            if g.tracks and represents_int(command):
                 try:
-                    track = tracks[int(command) - 1]
+                    track = g.tracks[int(command) - 1]
                     track_url = track.permalink_url
                     track_pic = track.artwork_url
                     track_title = track.title
                     cmd = which('mpv')
                     cmd += ' ' + track_url
-                    playWithMpv = PlayWithMpv(cmd)
-                    playWithMpv.start()
-                    print TerminalColors.BOLD + '"' + track_title + '"' + TerminalColors.ENDC + ' playing...'
+                    g.playWithMpv = PlayWithMpv(cmd)
+                    g.playWithMpv.start()
+                    print(TerminalColors.BOLD + '"' + track_title + '"' + TerminalColors.ENDC + ' playing...')
+                    g.isPlaying = True
 
                 except IndexError:
-                    print 'Please enter a valid number'
+                    print ('Please enter a valid number')
 
             elif command[0] == '/' and len(command) > 1:
-                tracks = client.get('/tracks', q=command[1:], limit=page_size)
-                for i in range(0, len(tracks) - 1):
+                g.tracks = client.get('/tracks', q=command[1:], limit=g.page_size)
+                for i in range(0, len(g.tracks) - 1):
                     formattedOrderNum = str(i + 1)
                     if len(formattedOrderNum) == 1:
                         formattedOrderNum = '0' + formattedOrderNum
-                    print TerminalColors.OKBLUE + formattedOrderNum + TerminalColors.ENDC + TerminalColors.OKGREEN + ' ' + \
-                          tracks[i].title + TerminalColors.ENDC
+                    print (
+                    TerminalColors.OKBLUE + formattedOrderNum + TerminalColors.ENDC + TerminalColors.OKGREEN + ' ' + \
+                    g.tracks[i].title + TerminalColors.ENDC)
 
             elif command == 'quit' or command == 'exit':
                 break
             else:
-                print 'Command not accepted'
+                print ('Command not accepted')
+
+    else:
+        if g.p:
+            if g.p.poll() is None:
+                if not g.elapsed_time:
+                    g.elapsed_time = 0
+
+                g.elapsed_time += 1
+                print(fmt_time(g.elapsed_time), end="\r")
+                sys.stdout.flush()
+                time.sleep(1)
+            else:
+                g.isPlaying = False
 
